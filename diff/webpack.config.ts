d635c24f9120b50c3f611dc84250c8c5ca7c92cb
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-01-07 18:17:27 +0900

    Fix typo

--- diff/webpack.config.ts
+++ diff/webpack.config.ts
@@ -1,5 +1,5 @@
 113c
-			format: chalk`  {cyan.bold 次利息の支払いに遅れたら海底散歩イン藍子ちゃんな} {bold [}:bar{bold ]} {green.bold :percent} {gray (:current/:total)} :elapseds`,
+			format: chalk`  {cyan.bold 次に利息の支払いの遅れたら海底散歩イン藍子ちゃんな} {bold [}:bar{bold ]} {green.bold :percent} {gray (:current/:total)} :elapseds`,
 .
 w
 q
